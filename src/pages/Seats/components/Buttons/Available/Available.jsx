38a8fdcfda60bbbd16009a2efdfdad89c4e193da
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-01-31 21:20:10 -0300

    Refator code verbosity

--- src/pages/Seats/components/Buttons/Available/Available.jsx
+++ src/pages/Seats/components/Buttons/Available/Available.jsx
@@ -1,25 +1,5 @@
-import { useMoviesContext } from "../../../../../contexts";
 import { Seat } from "./Styles";
 
-export function Available({ id, number, selected, setSelected }) {
-  const { movieData, setMovieData } = useMoviesContext();
-  
-  const handleSeatSelection = () => {
-    let newInfo = { ...movieData };
-
-    if (selected) {
-      newInfo.ids = newInfo.ids.filter((value) => value !== id);
-      newInfo.number = newInfo.number.filter((value) => value !== number);
-      setSelected(false);
-      setMovieData(newInfo);
-      return;
-    }
-
-    setSelected(!selected);
-    newInfo.ids.push(id);
-    newInfo.number.push(number);
-    setMovieData(newInfo);
-  };
-
+export function Available({ number, handleSeatSelection }) {
   return <Seat onClick={handleSeatSelection}>{number}</Seat>;
 }
