a467702fcd163abd5920600ae1023063e9348c41
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-01 13:33:36 -0300

    Creates 'Error' component

---
+++ src/pages/Seats/components/Forms/Error/Error.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./Styles";
+
+export function Error({ errMsg }) {
+  return <Container>{errMsg}</Container>;
+}
---
+++ src/pages/Seats/components/Forms/Error/Styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.span`
+  width: 100%;
+  height: 30px;
+  margin-top: 2.5px;
+  color: #ff0000;
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+export { Container };
